af5c227cff0ce0398ea3a3c34dbbd698c2fe55df
hodfords-phuong-vo-be <phuong.vt@hodfords.com>
2025-03-05 08:13:04 +0700

    feat: init detect change project

--- dist/main.js
+++ dist/main.js
@@ -50,10 +50,9 @@ const output_1 = require("./output");
 const yaml_helper_1 = require("./yaml.helper");
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        var _a;
         let config = {
             apps: core.getInput('apps').split(' '),
-            dependencies: ((_a = core.getInput('dependencies')) === null || _a === void 0 ? void 0 : _a.split(' ')) || [],
+            dependencies: core.getInput('dependencies') ? core.getInput('dependencies').split(' ') : [],
             workspacePath: core.getInput('workspacePath'),
             includePackage: core.getInput('includePackage'),
             yamlConfig: core.getInput('yamlConfig'),
